47cc0a8b3ee5c27ee919c3fcfe7f64b3ee0eb53d
Gaurav Jain <gauravjain6065@gmail.com>
2026-07-25 05:58:12 +0000

    Final updated

---
+++ crm_saas/project/doctype/crm_organization/crm_organization.py
@@ -0,0 +1,16 @@
+import frappe
+from frappe.model.document import Document
+from crm_saas.crm_saas.utils.normalize import normalize_email, normalize_phone
+
+
+class CRMOrganization(Document):
+	def validate(self):
+		self.email = normalize_email(self.email)
+		self.phone = normalize_phone(self.phone)
+		self.check_permissions_custom()
+
+	def check_permissions_custom(self):
+		if self.is_new():
+			return
+		if not frappe.has_permission(self.doctype, "write", self):
+			frappe.throw("Not permitted to modify this record")
