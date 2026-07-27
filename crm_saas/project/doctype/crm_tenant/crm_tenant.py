9b8b4a4f8aea94de4befa371109a84b64bb92076
Raj Sonar <rajsonar083@gmail.com>
2026-07-27 17:06:37 +0530

    update

--- crm_saas/project/doctype/crm_tenant/crm_tenant.py
+++ crm_saas/project/doctype/crm_tenant/crm_tenant.py
@@ -3,7 +3,17 @@
 
 import frappe
 from frappe.model.document import Document
-
+from crm_saas.utils.helpers import generate_site_name
 
 class CRMTenant(Document):
-	pass
+	def validate(self):
+		# Sync legacy slug field with tenant_slug
+		if not self.tenant_slug and self.slug:
+			self.tenant_slug = self.slug
+		elif not self.slug and self.tenant_slug:
+			self.slug = self.tenant_slug
+
+		if self.tenant_slug:
+			site_name = generate_site_name(self.tenant_slug)
+			self.site_name = site_name
+			self.primary_domain = site_name
